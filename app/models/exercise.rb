a6b8002be6ca6f02c0f024e1ba6f7a2a581b4225
André Moreira <36737050+andreLumor@users.noreply.github.com>
2022-03-28 15:20:04 -0300

    Task1.1 (#1)
    * Setup devise
    
    * Create models
    
    * Create model tests and fixtures
    
    * Reviewer changes

---
+++ app/models/exercise.rb
@@ -0,0 +1,8 @@
+class Exercise < ApplicationRecord
+  has_and_belongs_to_many :routines
+
+  validates :description, presence: true
+  validates :intensity, presence: true
+
+  validates_numericality_of :intensity, greater_than_or_equal_to: 0, less_than_or_equal_to: 10
+end
---
+++ spec/models/routine_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Routine, type: :model do
+  context 'associations' do
+    it { should have_and_belong_to_many(:exercises) }
+  end
+
+  context 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:exercises) }
+  end
+end
