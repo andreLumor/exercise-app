357e47a86cf7ff45d871221fbc12a6e6da9b103f
André Moreira <andre.moreira@codeminer42.com>
2022-03-29 12:36:07 -0300

    Create Routines feature

---
+++ app/controllers/routines_controller.rb
@@ -0,0 +1,40 @@
+class RoutinesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
+  # GET /routines or /routines.json
+  def index
+    @routines = Routine.all
+  end
+
+  # GET /routines/new
+  def new
+    @exercises = Exercise.all 
+    @routine = Routine.new
+  end
+
+  # POST /routines or /routines.json
+  def create
+    @routine = Routine.new(routine_params)
+
+    respond_to do |format|
+      if @routine.save
+        format.html { redirect_to routines_path, notice: "Routine was successfully created." }
+        format.json { render :show, status: :created, location: @routine }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @routine.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_routine
+      @routine = Routine.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def routine_params
+      params.require(:routine).permit(:name, exercise_ids: [])
+    end
+end
